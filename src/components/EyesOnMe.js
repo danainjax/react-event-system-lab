73e27259031fd41b7fa052111cd42fd2af871b7c
danainjax <danainjax@gmail.com>
2021-10-18 13:03:42 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  return (
+    <div>
+      <button onFocus={focusHandler} onBlur={blurHandler}>
+        RENDER
+      </button>
+    </div>
+  );
+}
+
+function focusHandler() {
+  console.log("Good!");
+}
+
+function blurHandler() {
+  console.log("Hey! Eyes on me!");
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+  return (
+    <div>
+      <h4>enter password</h4>
+      <input onKeyUp={passwordHandler} type="password" />
+    </div>
+  );
+}
+
+function passwordHandler() {
+  console.log("Entering password...");
+}
+
+export default Keypad;
